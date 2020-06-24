6ca912a08e08426c53951190344a4f3d44ebdc54
brianchristopherpeck <brianchristopherpeck@gmail.com>
2020-06-24 13:17:14 -0400

    selection sort

---
+++ sorting-algorithms/selection-sort/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func selectionSort(a []int, n int) {
+	// loop through the array
+	for i := 0; i < n -1; i++ {
+		// set the index to save the smallest value
+		var imin = i
+
+		// inner loop
+		for j := i + 1; j < n; j++ {
+			// if a[j] is less than current minimum, that becomes the current minumum
+			if a[j] < a[imin] {
+				imin = j
+			}
+		}
+		// save current value for swapping
+		var currentValue = a[i]
+
+		// swap with current value with current minimum
+		a[i] = a[imin]
+
+		// swap current minimum with current value
+		a[imin] = currentValue
+		fmt.Println(a)
+	}
+}
+
+func main() {
+	a := []int{2,7,4,1,5,3}
+	selectionSort(a, len(a))
+	fmt.Println(a)
+}
